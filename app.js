42cbff5458006cf2a10380366fe110488352e5fc
roudasaffuri <saffuri87@gmail.com>
2024-11-19 14:50:15 +0200

    middleware2

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bookPath= require("./routes/books");
 const authorPath = require("./routes/authors");
 const logger = require("./middlewares/logger");
+const {notFound , errorHandler} = require("./middlewares/errors");
 const dotenv = require("dotenv");
 dotenv.config();
 
@@ -11,15 +12,27 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URI)
     .then(()=>console.log("Connected To mondoDB..."))
     .catch((error)=> console.log("Connection Failed To MongoDB!",error));
+
+
 // Init App
 const app = express();
+
+
 // Apply middlwares
 app.use(express.json());
-
 app.use(logger);
+
+
 // Routes
 app.use("/api/books",bookPath)
 app.use("/authors",authorPath);
+
+
+// Error Handler Middleware
+app.use(notFound)
+app.use(errorHandler)
+
+
 
 //Running The Server
 const PORT = process.env.PORT || 8000;
